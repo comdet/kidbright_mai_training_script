ff5fa6805d993c09f06f8e4efa046c49092c4af4
comdet <listzone@hotmail.com>
2024-02-07 10:21:33 +0700

    update

--- main.py
+++ main.py
@@ -21,30 +21,6 @@ sys.path.append(".")
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/ping', methods=['GET'])
-def ping():
-    # return json 
-    return jsonify({'response': 'pong'})
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    if request.method == 'POST':
-        f = request.files['project']
-        f.save(f.filename)
-        return jsonify({'response': 'success'})
-
-@app.route('/train', methods=['POST'])
-def train():
-    if request.method == 'POST':
-        data = request.json
-        return jsonify({'response': 'success', 'data': data})
-    elif request.method == 'GET':
-        return jsonify({'response': 'success'})
-
 #==================================== Define Variables ====================================#
 UNAME = platform.uname()
 BACKEND = ""
@@ -82,6 +58,25 @@ app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 logging.basicConfig(level=logging.WARNING)
 logging.getLogger('werkzeug').setLevel(logging.WARNING)
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['project']
+        f.save(f.filename)
+        return jsonify({'response': 'success'})
+
+@app.route('/train', methods=['POST'])
+def train():
+    if request.method == 'POST':
+        data = request.json
+        return jsonify({'response': 'success', 'data': data})
+    elif request.method == 'GET':
+        return jsonify({'response': 'success'})
 
 @app.route('/ping', methods=["GET","POST"])
 def on_ping():
